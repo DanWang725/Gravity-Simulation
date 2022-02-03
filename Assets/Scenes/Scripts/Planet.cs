84f0f81204bd06a35dd58854da4dcb7c2a916d00
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-02-03 13:19:53 -0500

    added object properties to the variables

--- Assets/Scenes/Scripts/Planet.cs
+++ Assets/Scenes/Scripts/Planet.cs
@@ -8,6 +8,12 @@ public class Planet : MonoBehaviour
     decimal mass = 123;
     [SerializeField]
     LargeCoords _velocity;
+
+    public LargeCoords Position { get => _position; set => _position = value; }
+    public LargeCoords OldPos { get => _oldPos; set => _oldPos = value; }
+    public LargeCoords Velocity { get => _velocity; set => _velocity = value; }
+
     LargeCoords _position;
+
     LargeCoords _oldPos;
 }
