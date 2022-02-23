fe6a0d5d37e993d83058cc74acc92d8446bb1b13
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-02-23 16:19:38 -0500

    added a temporary event for sim speed slider

--- Assets/Scripts/Planets/EventManager.cs
+++ Assets/Scripts/Planets/EventManager.cs
@@ -7,11 +7,19 @@ namespace DanWang725.Planets
     public class EventManager : MonoBehaviour
     {
         public delegate void PauseButton();
-        public static event PauseButton OnPause;
 
+        public delegate void SliderValue(float val);
+        public static event PauseButton OnPause;
+        public static event SliderValue OnChange;
+    
         public void buttonPause()
         {
             OnPause();
+        }
+
+        public void sliderChange(float val)
+        {
+            OnChange(val);
         }
     }
 }
