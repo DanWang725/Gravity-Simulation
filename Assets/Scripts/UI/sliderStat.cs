b8fe7313bc8415b6e4d21f972f4092854a1dfe87
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-02-23 16:30:02 -0500

    created experimental slider script

---
+++ Assets/Scripts/UI/sliderStat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanWang725
+{
+    public class sliderStat : MonoBehaviour
+    {
+        public float simSpeed = 0.1f;
+        public void changeSimSpeed(float newVal)
+        {
+            simSpeed = newVal;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
