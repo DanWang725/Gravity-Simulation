64732225af5832a9546972b53463f25735416724
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-02-23 16:19:12 -0500

    added a constants class to avoid calculating values every update

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,10 @@
+namespace DanWang725
+{
+    public static class Constants
+    {
+        public static class Physics
+        {
+            public const decimal G = 0.0000000000667430M;
+        }
+    }
+}
